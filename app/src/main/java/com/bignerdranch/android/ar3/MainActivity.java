6784ee78d3a1299ff81ab86e47b29aa24e098839
linliu <1091640519@qq.com>
2017-11-25 21:41:45 +1000

    add the title and description

--- app/src/main/java/com/bignerdranch/android/ar3/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/ar3/MainActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.ar3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,11 +17,23 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         ImageView iv = (ImageView) findViewById(R.id.imageSwitcher2);
-
         TextView title=(TextView)findViewById(R.id.title);
-        title.setText("Ultra Marine Matrix              ");
-
         TextView tv =(TextView)findViewById(R.id.textView);
+        Button  button2018=(Button) findViewById(R.id.button2018);
+        Button  button2017=(Button) findViewById(R.id.button2017);
+        Button  button2016=(Button) findViewById(R.id.button2016);
+        Button  button2015=(Button) findViewById(R.id.button2015);
+
+
+        button2017.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+              }
+        });
+
+
+        title.setText("Ultra Marine Matrix              ");
         tv.setText("This turtle is on the verge of becoming a number in the matrix of extinct species. This artwork is to raise awareness and celebrate this stunning sea creature.");
     }
 }
