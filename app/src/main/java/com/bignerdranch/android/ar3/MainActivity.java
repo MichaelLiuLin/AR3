846b7e14c7b733974436c2d9c7929f9231e78fd3
linliu <1091640519@qq.com>
2017-11-25 21:07:52 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/ar3/MainActivity.java
@@ -0,0 +1,23 @@
+package com.bignerdranch.android.ar3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView) findViewById(R.id.imageSwitcher2);
+
+        TextView tv =(TextView)findViewById(R.id.textView);
+        tv.setText("This turtle is on the verge of becoming a number in the matrix of extinct species. This artwork is to raise awareness and celebrate this stunning sea creature.");
+    }
+}
